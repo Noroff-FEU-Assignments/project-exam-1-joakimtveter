f20a0f2ca56f12e0330ed657d5bdfb8a2d4c7829
Joakim Tveter <joakim@tveter.net>
2022-11-08 00:26:12 +0100

    Added single article

---
+++ javascript/pages/singleArticlePage.js
@@ -0,0 +1,39 @@
+import { showToast } from '../modules/toast.js';
+import { getValueFromURLParameter, enableMenuButtons } from '../modules/utils.js';
+import { postsEndpoint } from '../apiClient.js';
+
+const blogID = getValueFromURLParameter('id');
+
+if (!blogID) {
+    window.location.href = '/';
+}
+
+const articleHeading = document.getElementById('article-heading');
+const articleBody = document.getElementById('article-body');
+const articleCommentsSection = document.getElementById('article-comments-section');
+
+async function fetchSingleArticle(blogID) {
+    try {
+        let response = await fetch(postsEndpoint + '/' + blogID);
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        showToast('Unable to fetch article', 'error');
+        console.error(error);
+        setTimeout(() => {
+            window.location.href = '/';
+        }, 3500);
+    }
+}
+
+function renderSingleArticle(article) {
+    document.title = `${article.title.rendered} | Joakim Tveter - Thoughts on web development`;
+    articleHeading.innerHTML = article.title.rendered;
+    articleBody.innerHTML = article.content.rendered;
+    hljs.highlightAll();
+}
+
+const article = await fetchSingleArticle(blogID);
+renderSingleArticle(article);
+
+// enableMenuButtons();
