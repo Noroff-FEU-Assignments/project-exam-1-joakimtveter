356716e663a2f8f76188e5cd34f624335a3a80a6
Joakim Tveter <joakim@tveter.net>
2022-11-20 23:24:08 +0100

    end of day

--- javascript/modules/carousel.js
+++ javascript/modules/carousel.js
@@ -1,14 +1,18 @@
-const slidesContainer = document.querySelector('.post-slider__posts-container');
-const post = document.querySelector('.post-slider__post');
-const prevButton = document.querySelector('.post-slider__button.back');
-const nextButton = document.querySelector('.post-slider__button.next');
+function postSliderInit() {
+    const slidesContainer = document.querySelector('.post-slider__posts-container');
+    const post = document.querySelector('.post-slider__post');
+    const prevButton = document.querySelector('.post-slider__button.back');
+    const nextButton = document.querySelector('.post-slider__button.next');
 
-nextButton.addEventListener('click', () => {
-    const postWidth = post.clientWidth;
-    slidesContainer.scrollLeft += postWidth + 16;
-});
+    nextButton.addEventListener('click', () => {
+        const postWidth = post.clientWidth;
+        slidesContainer.scrollLeft += postWidth + 16;
+    });
 
-prevButton.addEventListener('click', () => {
-    const postWidth = post.clientWidth;
-    slidesContainer.scrollLeft -= postWidth + 16;
-});
+    prevButton.addEventListener('click', () => {
+        const postWidth = post.clientWidth;
+        slidesContainer.scrollLeft -= postWidth + 16;
+    });
+}
+
+export { postSliderInit };
--- javascript/pages/homePage.js
+++ javascript/pages/homePage.js
@@ -1,4 +1,56 @@
 import { showToast } from '../modules/toast.js';
 import { everyPageUtils } from '../modules/utils.js';
+import { postSliderInit } from '../modules/carousel.js';
+import { postsEndpoint } from '../apiClient.js';
 
+async function fetchLatestArticles() {
+    try {
+        let response = await fetch(`${postsEndpoint}?per_page=8&orderby=date&order=desc&_embed`);
+        let data = await response.json();
+        console.log(data);
+        renderLatestPosts(data);
+        postSliderInit();
+    } catch (error) {
+        showToast('Unable to fetch latest articles', 'error');
+        console.error(error);
+    }
+}
+
+function renderLatestPosts(posts) {
+    const postsContainer = document.querySelector('.post-slider__posts-container');
+    let postsHtml = '';
+    if (posts.length === 0) {
+        postsContainer.innerHTML = 'No posts found';
+        return;
+    }
+    posts.forEach((post) => {
+        let imageUrl = '';
+        let altText = '';
+        if (post._embedded['wp:featuredmedia']) {
+            imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
+            altText = post._embedded['wp:featuredmedia'][0].alt_text;
+        }
+        postsHtml += `
+        <li class='post-slider__post'>
+            <figure>
+                <img src= src="${imageUrl}" alt="${altText}" data-id="${post.id}" />
+            </figure>
+            <div class='post-slider__post-content'>
+                <h3 class='post-slider__post-title'>
+                    ${post.title.rendered}
+                </h3>
+                <p class="post-slider__post-meta">By: ${post._embedded.author[0].name} -  </p>
+                <p class='post-slider__post-excerpt'>
+                    ${post.excerpt.rendered}
+                </p>
+                <a class='post-slider__post-link' href='blog.html?id=${post.id}'>
+                    Read more
+                </a>
+            </div>
+        </li>`;
+    });
+    postsContainer.innerHTML = postsHtml;
+}
+
+fetchLatestArticles();
 everyPageUtils();
